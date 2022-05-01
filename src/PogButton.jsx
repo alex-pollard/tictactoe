eee4a2c2c7469158923afc2f1573cc96301010ac
alex-pollard <alex.pollard.1996@gmail.com>
2022-05-01 10:13:37 -0700

    hi

--- src/PogButton.jsx
+++ src/PogButton.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Button } from "antd";
 
 const PogButton = () => {
-  return <Button onClick={() => alert("POG")}>POG</Button>;
+  return (
+    <Button type="primary" onClick={() => alert("POG")}>
+      POG
+    </Button>
+  );
 };
 
 export default PogButton;
